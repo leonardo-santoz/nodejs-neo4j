96ebd662aa641008e128738e34b3bd3c0e149223
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2020-10-19 22:20:47 -0300

    neo4j config implemented

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
+const neo4j = require("neo4j-driver");
 
 const app = express();
 
-app.listen('3333', () => {
-    console.log('back-end started on port 3333 🚀')
+const neo4j_driver = neo4j.driver(
+  "bolt://neo4j+s://db-pgnuaxd3vf74xhslv0ca.graphenedb.com:24786",
+  neo4j.auth.basic("neo4j", "neo4j")
+);
+
+const session = neo4j_driver.session();
+
+app.listen("3333", () => {
+  console.log("back-end started on port 3333 🚀");
 });
-
-app.get('/movies', (request, response) => {
-    response.send('Hellooo');
-})
-
